03d2a3600edd50e69f91bfc330573d9c21cf14c1
Dininta Isnurthina <dini.dininta@gmail.com>
2021-03-20 17:04:26 +0700

    Create test and implementation for collectFollowingsNames function

--- src/instagram/InstagramService.js
+++ src/instagram/InstagramService.js
@@ -1,3 +1,5 @@
+import { sleep, pushIntoArray } from '../tools/ServerTool';
+
 export default class InstagramService {
   constructor(locals) {
     const { client } = locals;
@@ -11,5 +13,21 @@ export default class InstagramService {
   async getUserByUsername(data) {
     const { username } = data;
     return this._client.getUserByUsername({ username });
+  }
+
+  async _getFollowings(userId, after = null) {
+    return this._client.getFollowings({ userId, after });
+  }
+
+  async collectFollowingsNames(userId, verifiedAccOnly, names = [], endCursor = null) {
+    const action = `fetching following of user ${userId}`;
+    console.log(`${action}...`);
+    const { page_info: pageInfo, data } = await this._getFollowings(userId, endCursor);
+    const { has_next_page: hasNextPage, end_cursor: fetchedEndCursor } = pageInfo;
+    pushIntoArray(data, names, 'username', 'is_verified', verifiedAccOnly);
+    if (hasNextPage) {
+      sleep(3000);
+      await this.collectFollowingsNames(userId, verifiedAccOnly, names, fetchedEndCursor);
+    }
   }
 }
--- test/instagram/InstagramService.test.js
+++ test/instagram/InstagramService.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import InstagramService from '../../src/instagram/InstagramService';
 import WrongCredentialError from '../../src/error/WrongCredentialError';
 
@@ -10,12 +11,15 @@ describe('InstagramService', () => {
     client = {
       login: jest.fn(),
       getProfile: jest.fn(),
-      getUserByUsername: jest.fn()
+      getUserByUsername: jest.fn(),
+      getFollowings: jest.fn()
     };
     service = new InstagramService({ client });
+    jest.useFakeTimers();
   });
   afterEach(() => {
     jest.clearAllMocks();
+    jest.useRealTimers();
   });
   describe('#getProfile', () => {
     it('should return result of client.getProfile', async () => {
@@ -46,6 +50,54 @@ describe('InstagramService', () => {
       actualResult = await service.getUserByUsername(username);
 
       expect(actualResult).toEqual(username);
+    });
+  });
+  describe('#collectFollowingsNames', () => {
+    it('should return all followings of a user', async () => {
+      expectedResult = ['user2', 'user3', 'user4', 'user5'];
+      actualResult = [];
+
+      const userId = 1;
+      const verifiedAccOnly = false;
+      const firstFollowing = {
+        page_info: {
+          has_next_page: true,
+          end_cursor: 'asdaadada'
+        },
+        data: [
+          {
+            is_verified: true,
+            username: 'user2'
+          },
+          {
+            is_verified: false,
+            username: 'user3'
+          }
+        ]
+      };
+      client.getFollowings.mockResolvedValueOnce(firstFollowing);
+      const secondFollowing = {
+        page_info: {
+          has_next_page: false,
+          end_cursor: null
+        },
+        data: [
+          {
+            is_verified: true,
+            username: 'user4'
+          },
+          {
+            is_verified: false,
+            username: 'user5'
+          }
+        ]
+      };
+      client.getFollowings.mockResolvedValueOnce(secondFollowing);
+      jest.runAllTimers();
+
+      await service.collectFollowingsNames(userId, verifiedAccOnly, actualResult);
+
+      expect(actualResult).toEqual(expectedResult);
     });
   });
 });
