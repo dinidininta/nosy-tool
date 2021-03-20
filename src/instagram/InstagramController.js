f6ce25bf0903aaa2d9ece1f3c0be6e9f04fef50d
Dininta Isnurthina <dini.dininta@gmail.com>
2021-03-20 18:13:06 +0700

    Create POST endpoint to get all followings of a user. Change fetch user endpoint to GET.

--- src/instagram/InstagramController.js
+++ src/instagram/InstagramController.js
@@ -7,12 +7,14 @@ export default class InstagramController {
     this._router = express.Router();
     this._getProfile = this._getProfile.bind(this);
     this._getUserByUsername = this._getUserByUsername.bind(this);
+    this._getAllFollowingsOfUser = this._getAllFollowingsOfUser.bind(this);
   }
 
   registerRoutes() {
     this._app.use('/instagram', this._router);
     this._router.get('/profile', this._getProfile);
-    this._router.post('/user', this._getUserByUsername);
+    this._router.get('/user/:username', this._getUserByUsername);
+    this._router.post('/user/:username/followings', this._getAllFollowingsOfUser);
   }
 
   async _getProfile(req, res) {
@@ -23,7 +25,16 @@ export default class InstagramController {
 
   async _getUserByUsername(req, res) {
     const { instagramService } = this._app.locals.services;
-    const result = await instagramService.getUserByUsername(req.body);
+    const result = await instagramService.getUserByUsername(req.params);
+    return res.status(200).json(result);
+  }
+
+  async _getAllFollowingsOfUser(req, res) {
+    const result = [];
+    const { verifiedAccOnly } = req.body;
+    const { instagramService } = this._app.locals.services;
+    const { id: userId } = await instagramService.getUserByUsername(req.params);
+    await instagramService.collectFollowingsNames(userId, verifiedAccOnly, result);
     return res.status(200).json(result);
   }
 }
